7b377953d552d93b3e9592a83e8138f10f0d3619
Samuelcodingg <samuelroman39@gmail.com>
2022-02-09 22:38:49 -0500

    signin method implemented

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,10 +1,8 @@
 const { User } = require("../db/db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.register = (req, res) => {
-
-    console.log(User);
-
     //validate request
     if (!req.body.name) { 
         res.status(400).send({ message: "Name can not be empty" });
@@ -20,8 +18,6 @@ exports.register = (req, res) => {
         res.status(400).send({ message: "Password can not be empty" });
         return;
     }
-
-    
 
     //create user
     const user = {
@@ -44,4 +40,52 @@ exports.register = (req, res) => {
             res.status(500).send({ message: err.message });
         }
         );
+};
+
+exports.login = (req, res) => {
+    //validate request
+    if (!req.body.email) {
+        res.status(400).send({ message: "Email can not be empty" });
+        return;
+    }
+
+    if (!req.body.password) {
+        res.status(400).send({ message: "Password can not be empty" });
+        return;
+    }
+
+    //find user by email
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+        .then(user => {
+            //check if user exists
+            if (!user) {
+                res.status(401).send({ message: "Invalid Email or Password" });
+                return;
+            }
+
+            //check if password is correct
+            const passwordIsValid = bcrypt.compareSync(
+                req.body.password,
+                user.password
+            );
+
+            if (!passwordIsValid) {
+                res.status(401).send({ message: "Invalid Email or Password" });
+                return;
+            }
+
+            //if user is found and password is valid, create a token
+            const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+                expiresIn: 86400 // expires in 24 hours
+            });
+
+            res.status(200).send({ auth: true, token: token });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
 };
